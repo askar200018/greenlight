cab20c5f3c291057bb3cd5c577658320c7891f01
Askar Imagnali <imangali950@gmail.com>
2025-03-14 22:36:03 +0500

    feature: permissions based authorization

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -52,6 +52,12 @@ func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Reque
 		return
 	}
 
+	err = app.models.Permissions.AddForUser(user.ID, "movies:read")
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
 	token, err := app.models.Tokens.New(user.ID, 3*24*time.Hour, data.ScopeActivation)
 	if err != nil {
 		app.serverErrorResponse(w, r, err)
@@ -59,8 +65,8 @@ func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Reque
 	}
 
 	app.background(func() {
-		data := map[string]interface{}{"activationToken": token.Plaintext, "userID": user.ID}
-		err = app.mailer.Send(user.Email, "user_welcome.tmpl", data)
+		tmplData := map[string]interface{}{"activationToken": token.Plaintext, "userID": user.ID}
+		err = app.mailer.Send(user.Email, "user_welcome.tmpl", tmplData)
 		if err != nil {
 			app.logger.PrintError(err, nil)
 		}
